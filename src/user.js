8c19a973328570adff88be594fca3942a6a6986b
Pedro Martín González <pedromarting3@gmail.com>
2022-10-18 13:54:33 +0200

    creacion de clase user

---
+++ src/user.js
@@ -0,0 +1,19 @@
+/**
+ * Implementación de clase User
+ */
+
+class User
+{
+    /**
+     * Metodos
+     * @param {String} email 
+     * @param {Array} genres 
+     * @param {Array} films 
+     */
+    constructor(email,genres,films)
+    {
+        this.email = email;
+        this.genres = genres;
+        this.films = films;
+    }
+}
