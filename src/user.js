f5f58f990267b3617506ef6f446b2a7169f87d7c
Pedro Martín González <pedromarting3@gmail.com>
2022-10-19 17:11:24 +0200

    Update array films in class user

--- src/user.js
+++ src/user.js
@@ -8,7 +8,10 @@ class User
      * Metodos
      * @param {String} email 
      * @param {Array} genres 
+     * Array de Peliculas, la estructura del array es de la forma:
      * @param {Array} films 
+     * Donde: 
+     * films = [{name: 'Manolito Gafotas', date: 2014}, {name: 'Mafalda', date: 2017}];
      */
     constructor(email,genres,films)
     {
