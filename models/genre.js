b8f1ebfe742c4a23c0cf1336a09b58fc83990050
Pedro Martín González <pedromarting3@gmail.com>
2022-10-21 10:04:29 +0200

    Creacion del enumerado generos

---
+++ models/genre.js
@@ -0,0 +1,5 @@
+/**
+ * Implementación del enumerado de generos
+ */
+
+let genres = new Set(['Action','Thriller','War','Sci-Fi','Romance','Crime','Musical','Comedy','Horror','Documentary','Western','Fantasy']);
