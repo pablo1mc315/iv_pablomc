e8629bfd435b342d15ff859c541f20ab37257758
Pedro Martín González <pedromarting3@gmail.com>
2022-10-26 18:22:38 +0200

    Avanza #24 con la eliminación de genresSet

--- models/genresSet.js
+++
@@ -1,5 +0,0 @@
-/**
- * Implementación del enumerado de generos
- */
-const genresSet= new Set(['Accion','Thriller','Guerra','Ciencia-Ficcion','Romance','Crimen','Musical','Comedia','Terror','Documental','Western','Fantasia']);
-module.exports = genresSet;
