e93912fbcd2d848fbeedb664da3824785cbc27e4
pablo1mc315 <pablo1.lafuente@hotmail.com>
2022-11-26 14:11:09 +0100

    closes #34 #38 modifico iv.yaml y creo test de carga de biblioteca

---
+++ tests/test.js
@@ -0,0 +1,16 @@
+const assert = require('chai').assert;
+const user = require('../src/user');
+
+// Comprobaciones para la clase User
+describe('Clase User', function(){
+
+    // Testea que se haya cargado bien la biblioteca
+    describe('Cargar la biblioteca', function(){
+
+        it('Se ha cargado correctamente', function(){
+            assert.ok(user, "Cargado");
+        });
+    
+    });
+
+})
