267b6bd02373d3fc29ebb980eb2fbf9d1620f468
pablo1mc315 <pablo1.lafuente@hotmail.com>
2022-12-01 16:36:46 +0100

    closes #41 implementando la lógica de negocio

--- src/user.js
+++ src/user.js
@@ -4,13 +4,10 @@
 
 export default class User {
   /**
-     * Metodos
-     * @param {Array} fav_genres
-     * Array de Peliculas, la estructura del array es de la forma:
-     * @param {Array[Film]} films
-     * Donde:
-     * films = [{name: 'Manolito Gafotas', date: 2014}, {name: 'Mafalda', date: 2017}];
-     */
+    * Metodos
+    * @param {Array} fav_genres
+    * @param {Array[Film]} films
+    */
 
   constructor (fav_genres, films)
   {
@@ -19,5 +16,19 @@ export default class User {
 
     // Asignamos peliculas
     this.films = films
+  }
+
+  // Función que modifica el parametro films según el género de las películas
+  recommendByGenre (all_films)
+  {
+    // Las películas introducidas no pueden ser un vector nulo
+    if (all_films != null)
+    {
+      // Las películas que cumplan los requisitos las añadimos al vector de películas recomendadas
+      all_films.forEach(film => {
+        if (film.checkGenres(this))
+          this.films.push(film)
+      })
+    }
   }
 }
--- tests/test.js
+++ tests/test.js
@@ -44,10 +44,11 @@ describe('Clase User', function(){
     // Testea que el algoritmo de recomendaciones es correcto
     describe('Prueba de la lógica de negocio', function(){
 
-        it('El algoritmo ordena correctamente según los géneros favoritos del usuario', function(){
+        it('El algoritmo asigna correctamente según los géneros favoritos del usuario', function(){
             // Ajustar
             var fav_genres = ['Terror', 'Comedy']
-            var films = [
+            var films = []
+            var all_films = [                           // Creamos las películas disponibles
                 new Film(
                     "Annabelle",
                     2014,
@@ -55,21 +56,31 @@ describe('Clase User', function(){
                     ['Annabelle Wallis', 'Ward Horton', 'Alfre Woodard']
                 ),
                 new Film(
-                    "Hola",
-                    2011,
+                    "Inside Out",
+                    2015,
                     ['Comedy', 'Cartoon'],
-                    ['Yo', 'Tu']
+                    ['Phyllis Smith', 'Mindy Kaling']
+                ),
+                new Film(
+                    "El último duelo",
+                    2021,
+                    ['Action', 'Drama'],
+                    ['Lady Gaga', 'Clare Dunne', 'Bryony Hannah']
                 )
             ]
 
             var new_user = new User(fav_genres, films)
+            new_user.recommendByGenre(all_films)                // Realizamos la recomendación
 
             // Actuar
             var contenido = false
             new_user.films.forEach(film => contenido = film.checkGenres(new_user))
 
             // Afirmar            
-            expect(contenido).to.be.equal(true)      
+            expect(contenido).to.be.equal(true)                 // Solo se recomiendan dos películas
+            expect(new_user.films.length).to.be.equal(2)
+            expect(new_user.films[0].title).to.be.equal("Annabelle")
+            expect(new_user.films[1].title).to.be.equal("Inside Out")
         })
     })
 
