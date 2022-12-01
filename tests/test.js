aaa104fc5e3ec7fa3807e514a18911557b46bc93
pablo1mc315 <pablo1.lafuente@hotmail.com>
2022-12-01 16:50:21 +0100

    closes #41 modificando tests de carga de bibliotecas

--- tests/test.js
+++ tests/test.js
@@ -1,44 +1,33 @@
-import { assert } from 'chai';              // Biblioteca assert de Chai
-import { expect } from 'chai';              // Biblioteca expert de Chai
-import User from '../src/user.js';          // Clase User (entidad)
-import Film from '../src/film.js';          // Clase User (entidad)
+import { assert } from 'chai'              // Biblioteca assert de Chai
+import { expect } from 'chai'              // Biblioteca expert de Chai
+import User from '../src/user.js'          // Clase User (entidad)
+import Film from '../src/film.js'          // Clase User (entidad)
+
+
+// Comprobaciones para la clase Film
+describe('Clase Film', function(){
+
+    // Testea que se haya cargado bien la biblioteca
+    describe('Cargar la biblioteca', function(){
+        
+        it('Se ha cargado correctamente la clase Film', function(){
+            assert.ok(Film, "Cargado")
+        })
+    
+    })
+})
+
 
 // Comprobaciones para la clase User
 describe('Clase User', function(){
 
     // Testea que se haya cargado bien la biblioteca
     describe('Cargar la biblioteca', function(){
-
-        it('Se ha cargado correctamente', function(){
+        
+        it('Se ha cargado correctamente la clase User', function(){
             assert.ok(User, "Cargado")
-        });
-    
-    });
-
-    // Testea que el constructor funciona correctamente
-    describe('Prueba del constructor', function(){
-
-        it('Se ha creado el objeto User correctamente', function(){
-            // Ajustar
-            var fav_genres = ['Terror', 'Comedy']
-            var films = [
-                new Film(
-                    "Annabelle",
-                    2014,
-                    ['Terror'],
-                    ['Annabelle Wallis', 'Ward Horton', 'Alfre Woodard']
-                )
-            ]
-
-            // Actuar
-            var new_user = new User(fav_genres, films)
-
-            // Afirmar
-            expect(new_user.fav_genres.length).to.be.equal(2)
-            expect(new_user.fav_genres[0]).to.be.equal('Terror')
-            expect(new_user.fav_genres[1]).to.be.equal('Comedy')
-            expect(new_user.films.length).to.be.equal(1)
         })
+    
     })
 
     // Testea que el algoritmo de recomendaciones es correcto
