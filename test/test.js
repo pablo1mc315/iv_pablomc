9cc93b8481bb1727f2491c1d2428531993fc92ac
pablo1mc315 <pablo1.lafuente@hotmail.com>
2022-12-13 14:17:14 +0100

    closes #41 rehaciendo el test

--- test/test.js
+++ test/test.js
@@ -9,43 +9,29 @@ describe('Clase User', function(){
     // Testea que el algoritmo de recomendaciones es correcto
     describe('Prueba de la lógica de negocio', function(){
 
-        it('El algoritmo asigna correctamente según los géneros favoritos del usuario', function(){
+        it('El algoritmo asigna correctamente los géneros favoritos del usuario', function(){
             // Ajustar
-            var fav_genres = ['Terror', 'Comedy']
-            var films = []
-            var all_films = [                           // Creamos las películas disponibles
-                new Film(
-                    "Annabelle",
-                    2014,
-                    ['Terror'],
-                    ['Annabelle Wallis', 'Ward Horton', 'Alfre Woodard']
-                ),
-                new Film(
-                    "Inside Out",
-                    2015,
-                    ['Comedy', 'Cartoon'],
-                    ['Phyllis Smith', 'Mindy Kaling']
-                ),
-                new Film(
-                    "El último duelo",
-                    2021,
-                    ['Action', 'Drama'],
-                    ['Lady Gaga', 'Clare Dunne', 'Bryony Hannah']
-                )
+            var films = [                  // Asignamos películas vistas al usuario
+                new Film ("Annabelle", 2014, ['Terror'], ['Annabelle Wallis', 'Ward Horton', 'Alfre Woodard']),
+                new Film ("Inside Out", 2015, ['Comedy', 'Cartoon'], ['Phyllis Smith', 'Mindy Kaling']),
+                new Film ("El último duelo", 2021, ['Action', 'Drama'], ['Lady Gaga', 'Clare Dunne', 'Bryony Hannah']),
+                new Film ("Los hijos del mal", 1992, ['Terror', 'Drama'], ['Mel Harris', 'Dwight Schultz', 'Ashley Peldon']),
+                new Film ("Halloween con la familia Adams", 1977, ['Comedy', 'Terror'], ['John Astin', 'Ted Cassidy', 'Jackie Coogan']), 
+                new Film ("Dracula", 1974, ['Drama', 'Terror'], ['Jack Palance', 'Simon Ward', 'Nigel Davenport']), 
+                new Film ("Malnazidos", 2020, ['Terror', 'Comedy', 'Action'], ['Miki Esparbé', 'Aura Garrido', 'Luis Callejo']), 
+                new Film ("Ocho apellidos vascos", 2014, ['Comedy', 'Romance'], ['Clara Lago', 'Dani Rovira', 'Carmen Machi']) 
             ]
 
+            var fav_genres = []
             var new_user = new User(fav_genres, films)
-            new_user.recommendByGenre(all_films)                // Realizamos la recomendación
 
             // Actuar
-            var contenido = false
-            new_user.films.forEach(film => contenido = film.checkGenres(new_user))
+            new_user.setFavGenres()                 // Realizamos la recomendación por géneros
 
-            // Afirmar            
-            expect(contenido).to.be.equal(true)                 // Solo se recomiendan dos películas
-            expect(new_user.films.length).to.be.equal(2)
-            expect(new_user.films[0].title).to.be.equal("Annabelle")
-            expect(new_user.films[1].title).to.be.equal("Inside Out")
+            // Afirmar
+            expect(new_user.fav_genres.length).to.be.equal(2)
+            expect(new_user.fav_genres[0]).to.be.equal("Terror")
+            expect(new_user.fav_genres[1]).to.be.equal("Comedy")
         })
     })
 
