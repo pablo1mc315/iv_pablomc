24c08d73930ef02d3209d106aeba81532f5538f4
pablo1mc315 <pablo1.lafuente@hotmail.com>
2022-12-13 17:27:00 +0100

    closes #42 corrigiendo el algoritmo

--- src/user.js
+++ src/user.js
@@ -18,8 +18,9 @@ export default class User {
     this.films = films
   }
 
-  // Función que modifica el parametro fav_genres según las películas vistas por el usuario
-  // (voy a usar el algoritmo a priori de Argawal)
+
+  // Función que modifica los géneros favoritos del usuario según las películas vistas por el mismo
+  // (usando algoritmo 'a priori' de Agrawal)
   setFavGenres ()
   {
     // Umbral escogido (si la mitad de las películas vistas por el usuario contienen un género, se puede
@@ -27,20 +28,23 @@ export default class User {
     var umbral = parseInt(this.films.length/2)
 
     // Calculamos la frecuencia con la que un género aparece en las películas vistas por el usuario
-    var repsPorGenero = new Map()
+    var reps_por_genero = new Map()
 
     this.films.map(film => {
       // Una película puede tener varios géneros
       film.genres.map(genre => {
         // Si el género ya ha aparecido en alguna película, sumamos 1 repetición y, si no, lo añadimos
-        if (repsPorGenero.has(genre))
-          repsPorGenero[genre] += 1
+        if (reps_por_genero.has(genre))
+          reps_por_genero.set(genre, reps_por_genero.get(genre) + 1)
         else
-          repsPorGenero.set(genre, 1)
-      })
+          reps_por_genero.set(genre, 1)
+      })      
     })
 
     // Nos quedamos simplemente con los géneros que superan el umbral de repeticiones
-    this.fav_genres = repsPorGenero.keys().filter(i => { return repsPorGenero[i] >= umbral })
+    reps_por_genero.forEach((rep, genero) => {
+      if (rep >= umbral)
+        this.fav_genres.push(genero)
+    })
   }
 }
