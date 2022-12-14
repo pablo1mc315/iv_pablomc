1cee5f815595cc72b7576775714fb5f31dfa1ad9
pablo1mc315 <pablo1.lafuente@hotmail.com>
2022-12-14 16:27:34 +0100

    closes #43 rehaciendo los test actuales con la sintaxis de node:test

--- test/test.js
+++ test/test.js
@@ -1,10 +1,12 @@
-import { expect } from 'chai'              // Biblioteca expect de Chai
-import User from '../src/user.js'          // Clase User (entidad)
-import Film from '../src/film.js'          // Clase User (entidad)
+import assert from 'node:assert'                // Librería de aserciones estándar de Node
+import { describe, it } from 'node:test'
+
+import User from '../src/user.js'               // Clase User (entidad)
+import Film from '../src/film.js'               // Clase Film
 
 
 // Comprobaciones para la clase User
-describe('Clase User', function(){
+describe('Clase User', function() {
 
     // Testea que el algoritmo de recomendaciones es correcto
     describe('Prueba de la lógica de negocio', function(){
@@ -29,9 +31,9 @@ describe('Clase User', function(){
             new_user.setFavGenres()                 // Realizamos la recomendación por géneros
 
             // Afirmar
-            expect(new_user.fav_genres.length).to.be.equal(2)
-            expect(new_user.fav_genres[0]).to.be.equal("Terror")
-            expect(new_user.fav_genres[1]).to.be.equal("Comedy")
+            assert.equal(new_user.fav_genres.length, 2)
+            assert.equal(new_user.fav_genres[0], "Terror")
+            assert.equal(new_user.fav_genres[1], "Comedy")
         })
     })
 
